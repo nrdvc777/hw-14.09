6a1670ae1d4d388fa5f08b1cf1f40f51e6a272ae
nrdvc777 <135227299+nrdvc777@users.noreply.github.com>
2023-09-17 23:03:16 +0500

    'gap'

--- app1/models.py
+++ app1/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from datetime import datetime
+from django.contrib.auth.models import User
 # Create your models here.
+
 
 class TodoModel(models.Model):
     task_name = models.CharField(max_length=25, default='')
@@ -8,9 +10,10 @@ class TodoModel(models.Model):
     updated_at = models.DateTimeField(default=datetime.now)
     status = models.BooleanField(default=False)
     description = models.CharField(max_length=25, default='')
+    user = models.ForeignKey(User)
 
     def __str__(self) -> str:
         return self.task_name
     
-    class Meta:
-        db_table = 'Todos'
+    # class Meta:
+    #     db_table = 'Todos'
--- todo/urls.py
+++ todo/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/app1/', include('app1.urls'))
+    path('api/app1/', include('app1.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
